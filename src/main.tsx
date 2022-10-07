aef4dc0b5403b3a73592e3f101c129259230bc89
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-07 00:27:07 -0300

    feat: added theme and global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import { App } from '@/app'
+import { GlobalStyle, theme } from '@resources/styles/theme'
 
 ReactDOM.createRoot(document.querySelector('[data-js="root"]') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
