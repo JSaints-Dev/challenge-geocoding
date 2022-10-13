dee7503e1a91c62fc691eac6ff6912727e597783
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-13 00:08:38 -0300

    feat: moved address-box component to atoms components file

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './address-box/address-box.comp'
-export * from './address-box/address-box.types'
--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -6,3 +6,6 @@ export * from './button/button.types'
 
 export * from './divider/divider.comp'
 export * from './divider/divider.types'
+
+export * from './address-box/address-box.comp'
+export * from './address-box/address-box.types'
