195351b6869315475542c03c7c56c6e636c2b8b0
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-16 01:36:20 -0300

    feat: removed default background of app

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { Box } from '@ui/atoms'
 
 export const App = ({ children }: { children: React.ReactElement }) => {
   return (
-    <Box bg='primaryDark' w='100vw' minH='100vh'>
+    <Box w='100vw' minH='100vh'>
       {children}
     </Box>
   )
