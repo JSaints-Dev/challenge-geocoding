64c5521723a6c0315fde62e2b6ca2d774fc95227
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-15 21:57:18 -0300

    feat: updated border color of input component

--- src/ui/atoms/input/input.styles.ts
+++ src/ui/atoms/input/input.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const InputElement = styled.input`
   border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.colors.lightBlack};
+  border: 1px solid ${({ theme }) => theme.colors.primaryDark};
   height: 2.5rem;
   padding: ${({ theme }) => theme.spacing.sm};
   width: 100%;
