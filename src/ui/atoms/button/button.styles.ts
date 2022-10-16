407b7d247b3d44e52b2b5b7a9ea848299e062682
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-15 21:53:00 -0300

    refactor: prop renamed by project pattern

--- src/ui/atoms/button/button.styles.ts
+++ src/ui/atoms/button/button.styles.ts
@@ -2,8 +2,8 @@ import styled, { css } from 'styled-components'
 import { ButtonProps } from './button.types'
 
 export const ButtonElement = styled.button<Omit<ButtonProps, 'children'>>`
-${({ theme, backgroundColor = 'primary', fontSize = 'md', color = 'white' }) => css`
-  background-color: ${theme.colors[backgroundColor]};
+${({ theme, bg = 'primary', fontSize = 'md', color = 'white' }) => css`
+  background-color: ${theme.colors[bg]};
   border: none;
   border-radius: 4px;
   color: ${theme.colors[color]};
