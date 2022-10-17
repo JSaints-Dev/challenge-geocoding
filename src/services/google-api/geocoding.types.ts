6efae47885c60fa50bd73591fdda94efd89fed4c
JSaints-macbook16 <saintsjean.dev@gmail.com>
2022-10-17 11:41:18 -0300

    feat: added properties in geocoding api result typing

--- src/services/google-api/geocoding.types.ts
+++ src/services/google-api/geocoding.types.ts
@@ -1,11 +1,34 @@
 export type GeocodingResponse = {
   results: {
+    address_components: {
+      long_name: string
+      short_name: string
+      types: string[]
+    }[]
+    formatted_address: string,
     geometry: {
       location: {
         lat: number
         lng: number
       },
+      location_type: string,
+      viewport: {
+        northeast: {
+          lat: number,
+          lng: number,
+        },
+        southwest: {
+          lat: number,
+          lng: number,
+        },
+      },
+    },
+    plus_code: {
+      compound_code: string,
+      global_code: string,
     },
     place_id: string,
-  }[]
+    types: string[],
+  }[],
+  status: 'OK' | 'ZERO_RESULTS' | 'OVER_QUERY_LIMIT' | 'REQUEST_DENIED' | 'INVALID_REQUEST' | 'UNKNOWN_ERROR' | 'ERROR',
 }
